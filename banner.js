b0a3791c5156ff480919f523f6a6e5d8a3c9ba03
Muhammad Zain <zainarain2024@gmail.com>
2025-02-05 11:49:20 +0500

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+
+export function displayBanner() { console.log(chalk.magentaBright(`
+
+${chalk.bold.yellow('★ KALEIDO AUTO - BOT ★')}                
+${chalk.greenBright('░▀▀█░█▀█░▀█▀░█▀█')}
+${chalk.cyanBright('░▄▀░░█▀█░░█░░█░█')}
+${chalk.redBright('░▀▀▀░▀░▀░▀▀▀░▀░▀')}
+${chalk.bold.white('╔' + '═'.repeat(42) + '╗')}
+${chalk.bold.white('║')} ${chalk.bold.blueBright('👑 ZAIN ARAIN')} ${' '.repeat(26)}${chalk.bold.white('║')}
+${chalk.bold.white('║')} ${chalk.bold.greenBright('💻 AUTO SCRIPT MASTER')} ${' '.repeat(16)}${chalk.bold.white('║')}
+${chalk.bold.white('║')} ${chalk.bold.yellowBright('🚀 JOIN TELEGRAM NOW!')} ${' '.repeat(15)}${chalk.bold.white('║')}
+${chalk.bold.white('║')} ${chalk.underline.blueBright('🔗 https://t.me/AirdropScript6')} ${' '.repeat(7)}${chalk.bold.white('║')}
+${chalk.bold.white('║')} ${chalk.bold.cyanBright('@AirdropScript6 - OFFICIAL')} ${' '.repeat(10)}${chalk.bold.white('║')}
+${chalk.bold.white('║')} ${chalk.bold.redBright('📢 FAST - RELIABLE - SECURE')} ${' '.repeat(9)}${chalk.bold.white('║')}
+${chalk.bold.white('║')} ${chalk.bold.greenBright('🛠 SCRIPTS EXPERT')} ${' '.repeat(22)}${chalk.bold.white('║')}
+${chalk.bold.white('╚' + '═'.repeat(42) + '╝')}
+`));
+}
